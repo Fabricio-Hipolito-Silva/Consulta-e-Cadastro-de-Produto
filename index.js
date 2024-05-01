95bb3a36aa8ac2e03806c1850f4c8d84d638baa1
Fafas <fabriciohipvpaul@gmail.com>
2024-05-01 16:11:31 -0300

    Consulta e Cadastro
    Terminei o bruto da consulta e do cadastro do aplicativo, e funciona muito bem : )

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Permite a integração com o Node.js no processo do renderer
+      contextIsolation: false // Desativa a isolação de contexto, necessária se nodeIntegration está true
+  }
+  })
+
+  win.loadFile('paginas/consulta.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
